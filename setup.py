ca37d19def625ea899d52700b033c3daf6b809e6
Fernando Xavier <ferdxavier@gmail.com>
2024-12-11 17:22:30 -0300

    Update

--- setup.py
+++ setup.py
@@ -7,6 +7,20 @@ from setuptools.command.install import install
 class CustomInstallCommand(install):
     def run(self):
         install.run(self)
+        # Pega o diretorio bin 
+       
+        ANARCI_BIN = sys.executable
+        print(sys.version)
+        print(f'subprocess.__path__ --------> {self.exec_prefix}')
+        print(f'os.getgwd-----> { os.getcwd() } ')
+        print(f'sys.prefix -----> { sys.prefix } ')
+        print(f'sys.exec_prefix -----> {sys.exec_prefix } ')
+        print(f'sys.prefix + "/bin" -----> {sys.prefix + "/bin"}')
+        print(sys.prefix +'/lib/python' +sys.version[:3] +'/site-packages')
+        print(f'ANARCI_BIN ----: {ANARCI_BIN}')
+        print(f'site.getsitepackages()[0], "anarci")-----: {site.getsitepackages()[0]}')
+        print(f'ANARCI_BIN = sys.executable.split("python")[0]-----: {sys.executable}')
+        print('sys.prefix -----> {sys.prefix}')
       
 
 setup(name='anarci',
@@ -19,16 +33,14 @@ setup(name='anarci',
       package_dir={'anarci': 'lib/python/anarci'},
       # Copia os binário para o direto bin do ambiente
       data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS', 'bin/ANARCI']), 
-                     ('lib/python/anarci/dat/HMMs', os.listdir(os.getcwd() +'/lib/python/anarci/dat/HMMs'))],
-                     """
-                              ['lib/python/anarci/dat/HMMs/ALL.hmm', 
+                     ('lib/python/anarci/dat/HMMs', ['lib/python/anarci/dat/HMMs/ALL.hmm', 
                               'lib/python/anarci/dat/HMMs/ALL.hmm.h3f',
                               'lib/python/anarci/dat/HMMs/ALL.hmm.h3i',
                               'lib/python/anarci/dat/HMMs/ALL.hmm.h3m',
                               'lib/python/anarci/dat/HMMs/ALL.hmm.h3p'])],
-                     """
       include_package_data = True,
       # Coloca os binários no contexto da venv / env
       scripts=['bin/ANARCI'],
       cmdclass={"install": CustomInstallCommand, }, # Run post-installation routine
 )
+
